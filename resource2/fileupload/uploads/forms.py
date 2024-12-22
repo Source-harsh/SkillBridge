34f35cdf05b7f60b4ecfc9871145cb8304e3036a
Source-harsh <harsh2911206@gmail.com>
2024-12-22 15:27:59 +0530

    Delete resource2/fileupload directory

--- resource2/fileupload/uploads/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import SubjectFile
-
-class SubjectFileForm(forms.ModelForm):
-    class Meta:
-        model = SubjectFile
-        fields = ['subject', 'file']
--- resource2/fileupload/uploads/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class SubjectFile(models.Model):
-    subject = models.CharField(max_length=100)
-    file = models.FileField(upload_to='uploads/')
-
-    def __str__(self):
-        return self.subject
