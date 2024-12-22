7c23bbb0312d65a695b8a934c27ba5337ab86e61
Source-harsh <harsh2911206@gmail.com>
2024-12-22 11:45:05 +0530

    Add files via upload

---
+++ home/urls.py
@@ -0,0 +1,33 @@
+"""
+URL configuration for skill_gap project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from home import views
+
+urlpatterns = [
+    path('',views.index,name='index'),
+    path('quiz/',views.quiz,name='quiz'),
+    path('resource/',views.resource,name='resources'),
+    path('query/',views.query,name='query'),
+    path('signup/',views.signup,name='signup'),
+    path('login/',views.login_view,name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('modify_profile/', views.modify_profile, name='modify_profile'),
+    path('accept_request/<int:request_id>/', views.accept_request, name='accept_request'),
+    path('reject_request/<int:request_id>/', views.reject_request, name='reject_request'),
+    path('project_collab/', views.project_collaboration, name='project_collab'),
+]
